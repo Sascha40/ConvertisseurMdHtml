ce7cc1fc3b1300d2bff7dd11a600961a0f5b0426
Sascha Sallès <43401678+Sascha40@users.noreply.github.com>
2019-02-11 22:37:41 +0100

    Update convertisseurOpenSource.py

--- convertisseurOpenSource.py
+++ convertisseurOpenSource.py
@@ -2,7 +2,7 @@ listeHtml = ['<h1>','<h2>','<h3>','<em>','</em>','<ul>','<li>','<a>','</a>','<br
 listeMd = ['# ','## ','### ','**','**','* ','	*','[',']','\n','[',']','__','__','**','**']
 listeNonDésirés = ['<!DOCTYPE', 'html>','<html>','<head>','</head>','</h1>','</h2>','</h3>','</li>','<p>','</p>''</img>','href=','</ul>','</body>','</html>','<body>','<title>','</title>','</ol>']
 with open(input("Veuillez entrez le chemin de votre fichier .html à convertir en markdown: ")) as fichierHtml:
-	fichier = open('Fichier Converti vers Markdo.md','w')
+	fichier = open('Fichier_Converti_vers_Markdown.md','w')
 	for lignes in fichierHtml:
 		lignes = lignes.replace(">", "> ")
 		lignes = lignes.replace("><", "> <")
@@ -21,3 +21,20 @@ with open(input("Veuillez entrez le chemin de votre fichier .html à convertir e
 		fichier.write(chaine)
 #		print(chaine)
 	
+    '''
+    Copyright (C) <2019>  <Sallès Sascha>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    '''
