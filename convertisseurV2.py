167838f485714ad76e4f81a1aac26b6c9569d72d
Sascha Sallès <43401678+Sascha40@users.noreply.github.com>
2019-02-12 11:52:15 +0100

    Update convertisseurV2.py

--- convertisseurV2.py
+++ convertisseurV2.py
@@ -1,6 +1,7 @@
 from markdown import *
-fichiermd = open(input("Entrez le chemin de votre fichier markdown à convertir en HTML")) 
-fichierHTML = open('fichier_converti_en_html.html','w') 
+fichiermd = open(input("Entrez le chemin de votre fichier markdown à convertir en HTML :")) 
+fichierHTML = open('fichier_converti_en_html.html','w')
+fichierHTML.write('<meta charset="UTF-8">')
 for ligne in fichiermd:    
 	html = markdown(ligne)  
 	fichierHTML.write(html) 
